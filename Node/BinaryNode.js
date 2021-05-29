6320b0fac48a61d8ca778b1748a31260882aaecb
chicichich <pch6789@naver.com>
2021-05-29 22:32:38 +0900

    Implement: BinaryNode

---
+++ Node/BinaryNode.js
@@ -0,0 +1,28 @@
+const BinaryNode = {
+  init: function init(source) {
+    source = Object.assign(
+      { parent: null, left: null, right: null, data: null },
+      source
+    );
+    if (
+      !this._canLinked(source.parent) ||
+      !this._canLinked(source.left) ||
+      !this._canLinked(source.right)
+    ) {
+      throw new Error("Other than BinaryNode, You cannot link");
+    }
+    this.parent = source.parent;
+    this.left = source.left;
+    this.right = source.right;
+    this.data = source.data;
+    return Object.seal(this);
+  },
+  _canLinked: function _canLinked(target) {
+    if (target === null || BinaryNode.isPrototypeOf(target)) {
+      return true;
+    }
+    return false;
+  },
+};
+
+export default BinaryNode;
