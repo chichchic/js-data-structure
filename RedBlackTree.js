3f89471825ddd1169189340ecabe8cb9ea0f00b1
chicichich <pch6789@naver.com>
2021-06-03 15:44:08 +0900

    Implement: RedBalckTree - 삭제 구현

--- RedBlackTree.js
+++ RedBlackTree.js
@@ -7,7 +7,7 @@ const RedBlackNode = Object.create(BinaryNode, {
   isRed: { value: true, writable: true },
 }).init();
 const nill = Object.create(RedBlackNode).init();
-nill.isRed = false;
+Object.defineProperty(nill, "isRed", { value: false });
 RedBlackNode.left = nill;
 RedBlackNode.right = nill;
 
@@ -30,10 +30,13 @@ const RedBlackTree = {
     return this._size;
   },
   has: function has(data) {
+    return !!this.find(data);
+  },
+  find: function find(data) {
     let cursor = this._root;
     while (cursor !== nill) {
       if (cursor.data === data) {
-        return true;
+        return cursor;
       }
       cursor = this._compareFunc(cursor.data, data)
         ? cursor.left
@@ -175,7 +178,127 @@ const RedBlackTree = {
       ? node.parent.parent.right
       : node.parent.parent.left;
   },
-  remove: function remove() {},
+  remove: function remove(node) {
+    //참고 링크 : https://ict-nroo.tistory.com/73
+    this._size--;
+    let deletedNode, targetNode;
+    if (node.left === nill && node.right === nill) {
+      deletedNode = node;
+    } else if (node.right === nill) {
+      deletedNode = this._findBiggestChild(node.left);
+    } else if (node.left === nill) {
+      deletedNode = this._findSmallestChild(node.right);
+    } else {
+      const leftCandidate = this._findBiggestChild(node.left);
+      if (leftCandidate.isRed) {
+        deletedNode = leftCandidate;
+      }
+      const rightCandidate = this._findSmallestChild(node.right);
+      if (rightCandidate.isRed) {
+        deletedNode = rightCandidate;
+      }
+      if (deletedNode === undefined) {
+        deletedNode = rightCandidate;
+      }
+    }
+    if (deletedNode.left !== nill) {
+      targetNode = deletedNode.left;
+    } else {
+      targetNode = deletedNode.right;
+    }
+    if (deletedNode === this._root) {
+      this._root = targetNode;
+      targetNode.parent === null;
+    } else {
+      const deletedNodeparent = deletedNode.parent;
+      targetNode.parent = deletedNodeparent;
+      const deletedNodeDirection =
+        deletedNodeparent.left === deletedNode ? "left" : "right";
+      deletedNodeparent[deletedNodeDirection] = targetNode;
+    }
+    node.data = deletedNode.data;
+    if (deletedNode.isRed === true) {
+      return true;
+    }
+    if (targetNode === nill) {
+      return true;
+    }
+    return this._fixupDelete(targetNode);
+  },
+  _fixupDelete: function _fixupDelete(node) {
+    while (true) {
+      if (node === this._root) {
+        node.isRed = false;
+        return true;
+      }
+      const parent = node.parent;
+      const isLeftChild = parent.left === node ? true : false;
+      const sibling = isLeftChild ? parent.right : parent.left;
+      //case: 1
+      if (sibling.isRed) {
+        parent.isRed = true;
+        sibling.isRed = false;
+        if (isLeftChild) {
+          this._rotateLeft(parent);
+        } else {
+          this._rotateRight(parent);
+        }
+        continue;
+      }
+      //case: 3
+      if (isLeftChild && sibling.left.isRed) {
+        sibling.isRed = true;
+        sibling.left.isRed = false;
+        this._rotateRight(sibling);
+        continue;
+      }
+      //case: 7
+      if (!isLeftChild && sibling.right.isRed) {
+        sibling.isRed = true;
+        sibling.right.isRed = false;
+        this._rotateLeft(sibling);
+        continue;
+      }
+      //case: 4
+      if (isLeftChild && sibling.right.isRed) {
+        sibling.isRed = parent.isRed;
+        parent.isRed = false;
+        sibling.right.isRed = false;
+        this._rotateLeft(parent);
+        return true;
+      }
+      //case: 8
+      if (!isLeftChild && sibling.left.isRed) {
+        sibling.isRed = parent.isRed;
+        parent.isRed = false;
+        sibling.left.isRed = false;
+        this._rotateRight(parent);
+        return true;
+      }
+      //case: 2, 5
+      sibling.isRed = true;
+      if (!parent.isRed) {
+        node = parent;
+        continue;
+      }
+      parent.isRed = false;
+      return true;
+    }
+  },
+  _findBiggestChild: function _findBiggestChild(node) {
+    let cursor = node;
+    while (cursor.right !== nill) {
+      cursor = cursor.right;
+    }
+    return cursor;
+  },
+  _findSmallestChild: function _findSmallestChild(node) {
+    let cursor = node;
+    while (cursor.left !== nill) {
+      cursor = cursor.left;
+    }
+    return cursor;
+  },
 };
 
 export default RedBlackTree;
