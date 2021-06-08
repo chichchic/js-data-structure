2a903ed1dcf18f4d3287ef0cce24237d9ad26e32
chicichich <pch6789@naver.com>
2021-06-08 21:09:13 +0900

    Update: 234Node에 parent 추가 및 insert 메서드 변경

--- Node/234Node.js
+++ Node/234Node.js
@@ -2,15 +2,29 @@ import LinkedNode from "./LinkedNode.js";
 
 const ttfNode = {
   init: function init(source) {
+    if (typeof source.compareFunc !== "function") {
+      throw new Error(
+        "When creating the 2-3-4 Node, you need to declare 'compareFunc' function"
+      );
+    }
     source = Object.assign({ parent: null, isLeaf: false }, source);
+    this.compareFunc = source.compareFunc;
     this.isLeaf = source.isLeaf;
+    this.parent = source.parent;
     this.keys = [];
-    this.children = [null, null, null, null];
     return this;
   },
   insert: function insert(data) {
     const newNode = Object.create(LinkedNode).init({ data });
-    this.keys.push(newNode);
+    const spliceIndex = this.keys.findIndex((val) =>
+      this.compareFunc(data, val.data)
+    );
+    if (spliceIndex < 0) {
+      this.keys.push(newNode);
+      return this.size - 1;
+    }
+    this.keys.splice(spliceIndex, 0, newNode);
+    return spliceIndex;
   },
   _canLinked: function _canLinked(target) {
     if (target === null || ttfNode.isPrototypeOf(target)) {
