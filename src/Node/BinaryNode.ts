1811c83b38f2aa213b0c452cd62f844f004735e9
chicichich <pch6789@naver.com>
2021-11-04 21:40:49 +0900

    fix: pointer had set wrong type value

--- src/Node/BinaryNode.ts
+++ src/Node/BinaryNode.ts
@@ -1,29 +1,29 @@
 import { pointer } from "Global";
 export default class BinaryNode<T> {
-  private _parent: pointer<T>;
-  private _left: pointer<T>;
-  private _right: pointer<T>;
+  private _parent: pointer<T, BinaryNode<T>>;
+  private _left: pointer<T, BinaryNode<T>>;
+  private _right: pointer<T, BinaryNode<T>>;
   constructor() {
     this._parent = null;
     this._left = null;
     this._right = null;
   }
-  public get parent(): pointer<T> {
+  public get parent(): pointer<T, BinaryNode<T>> {
     return this._parent;
   }
-  public set parent(value: pointer<T>) {
+  public set parent(value: pointer<T, BinaryNode<T>>) {
     this._parent = value;
   }
-  public get left(): pointer<T> {
+  public get left(): pointer<T, BinaryNode<T>> {
     return this._left;
   }
-  public set left(value: pointer<T>) {
+  public set left(value: pointer<T, BinaryNode<T>>) {
     this._left = value;
   }
-  public get right(): pointer<T> {
+  public get right(): pointer<T, BinaryNode<T>> {
     return this._right;
   }
-  public set right(value: pointer<T>) {
+  public set right(value: pointer<T, BinaryNode<T>>) {
     this._right = value;
   }
 }
--- src/Node/Cursor.ts
+++ src/Node/Cursor.ts
@@ -1,21 +1,21 @@
 import { pointer } from "Global";
 export default class Cursor<T> {
-  protected prev: pointer<T>;
-  protected next: pointer<T>;
+  protected prev: pointer<T, Cursor<T>>;
+  protected next: pointer<T, Cursor<T>>;
   constructor() {
     this.prev = null;
     this.next = null;
   }
-  getPrev(): pointer<T> {
+  getPrev(): pointer<T, Cursor<T>> {
     return this.prev;
   }
-  setPrev(newPrev: pointer<T>): void {
+  setPrev(newPrev: pointer<T, Cursor<T>>): void {
     this.prev = newPrev;
   }
-  getNext(): pointer<T> {
+  getNext(): pointer<T, Cursor<T>> {
     return this.next;
   }
-  setNext(newNext: pointer<T>): void {
+  setNext(newNext: pointer<T, Cursor<T>>): void {
     this.next = newNext;
   }
 }
