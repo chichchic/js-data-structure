35a6794214854947f9006ae94ba80e901e210e6d
chicichich <pch6789@naver.com>
2021-11-05 16:11:05 +0900

    feat: modify BinaryNode constructor

--- src/Node/BinaryNode.ts
+++ src/Node/BinaryNode.ts
@@ -1,14 +1,20 @@
 import { pointer } from "Global";
+interface node<T> {
+  parent?: pointer<BinaryNode<T>>;
+  left?: pointer<BinaryNode<T>>;
+  right?: pointer<BinaryNode<T>>;
+  data: T;
+}
 export default class BinaryNode<T> {
   protected _parent: pointer<BinaryNode<T>>;
   protected _left: pointer<BinaryNode<T>>;
   protected _right: pointer<BinaryNode<T>>;
   protected _data: T;
-  constructor(data: T) {
-    this._parent = null;
-    this._left = null;
-    this._right = null;
-    this._data = data;
+  constructor(source: node<T>) {
+    this._parent = source.parent === undefined ? null : source.parent;
+    this._left = source.left === undefined ? null : source.left;
+    this._right = source.right === undefined ? null : source.right;
+    this._data = source.data;
   }
   public get parent(): pointer<BinaryNode<T>> {
     return this._parent;
