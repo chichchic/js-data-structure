e15eb0499f11e1280e6a0a58d53e84562801994c
chicichich <pch6789@naver.com>
2021-11-08 21:15:11 +0900

    feat: append test code and fix miss typo

--- test/BinarySearchTree.test.ts
+++ test/BinarySearchTree.test.ts
@@ -12,33 +12,44 @@ describe("BinarySearchTree", () => {
       if (b === null) {
         return false;
       }
-      return a < b;
+      return a > b;
     });
+    bst.insert(7);
     bst.insert(1);
     bst.insert(3);
     bst.insert(5);
-    bst.insert(7);
+    bst.insert(2);
+    bst.insert(4);
+    bst.insert(8);
+    bst.insert(6);
+    bst.insert(-1);
+    bst.insert(-9);
   });
   it("sould insert increase size", () => {
-    expect(bst.size).to.equal(4);
+    const beforeSize = bst.size;
     bst.insert(9);
-    expect(bst.size).to.equal(5);
+    expect(bst.size - beforeSize).to.equal(1);
   });
-  it("sould remove decrease size when has data", () => {
-    expect(bst.size).to.equal(4);
+  it("sould remove decrease size when have data", () => {
+    const beforeSize = bst.size;
     bst.remove(1);
-    expect(bst.size).to.equal(3);
+    expect(bst.size - beforeSize).to.equal(-1);
   });
-  it("sould remove return true when has data", () => {
+  it("sould remove return true when have data", () => {
     expect(bst.remove(7)).to.be.true;
+  });
+  it("sould remove data when have data", () => {
+    expect(bst.have(1)).to.be.true;
+    expect(bst.remove(1)).to.be.true;
+    expect(bst.have(1)).to.be.false;
   });
   it("sould remove return fale when does'nt have data", () => {
     expect(bst.remove(9)).to.be.false;
   });
-  it("should have return true when has data", () => {
+  it("should have return true when have data", () => {
     expect(bst.have(1)).to.be.true;
   });
-  it("should have return true when dosen't have data", () => {
+  it("should have return false when dosen't have data", () => {
     expect(bst.have(11)).to.be.false;
   });
 });
