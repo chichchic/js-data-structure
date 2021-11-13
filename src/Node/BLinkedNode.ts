15dac23b70d0d4ae8115e4815b8e24306fb02d32
chicichich <pch6789@naver.com>
2021-11-13 14:01:50 +0900

    feat: implement BlinkedNode

---
+++ src/Node/BLinkedNode.ts
@@ -0,0 +1,33 @@
+import keyIndex from "@src/Node/keyIndex";
+import BNode from "@src/Node/BNode";
+
+interface node<T, U> {
+  prev?: BNode<T, U>;
+  next?: BNode<T, U>;
+  data: keyIndex<T, U>;
+}
+export default class BLinkedNode<T, U> {
+  private _data: keyIndex<T, U>;
+  private _prev: BNode<T, U> | null;
+  private _next: BNode<T, U> | null;
+  constructor(source: node<T, U>) {
+    this._data = source.data;
+    this._prev = source.prev == undefined ? null : source.prev;
+    this._next = source.next == undefined ? null : source.next;
+  }
+  public get data(): keyIndex<T, U> {
+    return this._data;
+  }
+  public get prev(): BNode<T, U> | null {
+    return this._prev;
+  }
+  public set prev(value: BNode<T, U> | null) {
+    this._prev = value;
+  }
+  public get next(): BNode<T, U> | null {
+    return this._next;
+  }
+  public set next(value: BNode<T, U> | null) {
+    this._next = value;
+  }
+}
