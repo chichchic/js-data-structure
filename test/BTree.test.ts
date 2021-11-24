e78bcfa8c37c0be2a5e468714cbc546cf0f952f4
chicichich <pch6789@naver.com>
2021-11-24 15:56:07 +0900

    refactor: Btree test simplify

--- test/BTree.test.ts
+++ test/BTree.test.ts
@@ -4,6 +4,8 @@ import { beforeEach, it } from "mocha";
 
 describe("BTree", () => {
   let bt: BTree<number, number>;
+  const insertData = [11, 1, 4, 6, 3, 10, 8, 5, 7, 2, 9];
+  const sortedData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   beforeEach(() => {
     bt = new BTree<number, number>((a, b) => a < b);
   });
@@ -15,76 +17,43 @@ describe("BTree", () => {
     expect(bt.isEmpty()).to.be.false;
   });
   it("getIndex method return index when key is matched", () => {
-    bt.insert(11, 11);
-    bt.insert(1, 1);
-    bt.insert(4, 4);
-    bt.insert(6, 6);
-    bt.insert(3, 3);
-    bt.insert(10, 10);
-    bt.insert(8, 8);
-    bt.insert(5, 5);
-    bt.insert(7, 7);
-    bt.insert(2, 2);
-    bt.insert(9, 9);
-    expect(bt.getIndex(6)).to.equal(6);
+    insertData.forEach((v) => {
+      bt.insert(v, v);
+    });
+    sortedData.forEach((v) => {
+      expect(bt.getIndex(v)).to.equal(v);
+    });
   });
   it("getIndex method return null when key is not matched key", () => {
-    bt.insert(11, 11);
-    bt.insert(1, 1);
-    bt.insert(4, 4);
-    bt.insert(6, 6);
-    bt.insert(3, 3);
-    bt.insert(10, 10);
-    bt.insert(8, 8);
-    bt.insert(5, 5);
-    bt.insert(7, 7);
-    bt.insert(2, 2);
-    bt.insert(9, 9);
+    insertData.forEach((v) => {
+      bt.insert(v, v);
+    });
     expect(bt.getIndex(12)).to.equal(null);
   });
   it("insert method increase size", () => {
     expect(bt.size).to.equal(0);
-    bt.insert(11, 11);
-    bt.insert(1, 1);
-    bt.insert(4, 4);
-    bt.insert(6, 6);
-    bt.insert(3, 3);
-    bt.insert(10, 10);
-    bt.insert(8, 8);
-    bt.insert(5, 5);
-    bt.insert(7, 7);
-    bt.insert(2, 2);
-    bt.insert(9, 9);
+    insertData.forEach((v) => {
+      bt.insert(v, v);
+    });
     expect(bt.size).to.equal(11);
   });
+  it("insert method return true", () => {
+    insertData.forEach((v) => {
+      expect(bt.insert(v, v)).to.be.true;
+    });
+  });
   it("remove method decrease size", () => {
-    bt.insert(11, 11);
-    bt.insert(1, 1);
-    bt.insert(4, 4);
-    bt.insert(6, 6);
-    bt.insert(3, 3);
-    bt.insert(10, 10);
-    bt.insert(8, 8);
-    bt.insert(5, 5);
-    bt.insert(7, 7);
-    bt.insert(2, 2);
-    bt.insert(9, 9);
+    insertData.forEach((v) => {
+      bt.insert(v, v);
+    });
     expect(bt.size).to.equal(11);
     bt.remove(7);
     expect(bt.size).to.equal(10);
   });
   it("remove method delete node", () => {
-    bt.insert(11, 11);
-    bt.insert(1, 1);
-    bt.insert(4, 4);
-    bt.insert(6, 6);
-    bt.insert(3, 3);
-    bt.insert(10, 10);
-    bt.insert(8, 8);
-    bt.insert(5, 5);
-    bt.insert(7, 7);
-    bt.insert(2, 2);
-    bt.insert(9, 9);
+    insertData.forEach((v) => {
+      bt.insert(v, v);
+    });
     expect(bt.getIndex(11)).to.equal(11);
     bt.remove(11);
     expect(bt.getIndex(11)).to.equal(null);
